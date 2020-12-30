fca9dd8ffe95fbcb37793f9e0a1de3b85c91f1ee
Frederico Santos <fredericosantos@github.com>
2020-12-30 00:36:55 +0000

    started work on imputing

--- helper_functions.py
+++ helper_functions.py
@@ -16,3 +16,23 @@ def check_inconsistency(df, col1, col2):
 def plotly_dist(df, col1):
     fig = px.histogram(df, x=col1, marginal="violin", template="ggplot2", color_discrete_sequence=colors)
     fig.show()
+    
+    
+    
+    
+def entity_embeddings(learner, cat_names, xs, valid_xs, test_xs, full_xs, all_xs):
+    for xs_emb in [xs, valid_xs, test_xs, full_xs, all_xs]:
+        for n, cat in enumerate(cat_names):
+            cat_embedding = to_np(learner.model.embeds[n].weight)
+            for i in range(cat_embedding.shape[1]):
+                xs_emb[cat+"_emb"+str(i)] = xs_emb[cat].replace({j: cat_embedding[j, i] for j in range(cat_embedding.shape[0])})
+            xs_emb.drop(columns=cat, inplace=True)
+            
+            
+            
+            
+            
+def save_embeddings(learner, cat_names, name):
+    for i, cat in enumerate(cat_names):
+        w = to_np(learner.model.embeds[i].weight)
+        np.save(f"embeddings/{name}_{cat}", w)
